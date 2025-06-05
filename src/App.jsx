e84f336a13cad62dc9e6322257731a35803248da
Dario <darioriveraharo@gmail.com>
2025-06-05 23:44:51 +0000

    Agregar el boton de like

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 const Card = ({ title }) => {
+
+  const [hasLiked, setHasLiked] = useState(false);
+
   return (
-    <div style={{
-      border: '1px solid #4b5362',
-      padding: '20px',
-      margin: '10px',
-      backgroundColor: '#31363h' 
-    }}>
+    <div className='card'>
       <h2>{title}</h2>
+
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? '❤️' : '🤍'}
+      </button>
     </div>
   )
 }
 
 const App = () => {
+
   return (
     <div className='card-container'> 
       <Card title="Star Wars" rating={5} isCool={true}/>
