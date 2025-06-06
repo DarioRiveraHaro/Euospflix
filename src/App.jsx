4e4c5e9f39dc7f861a892670716f24c5d25f85ec
Dario <darioriveraharo@gmail.com>
2025-06-06 21:43:13 +0000

    Agregar el componente spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react"
 import Search from "./components/Search"
+import Spinner from "./components/Spinner"
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -16,10 +17,14 @@ const API_OPTIONS = {
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [movieList,setMovieList] = useState([]);
+  const [isLoading,setIsLoading] = useState(false);
 
-  
 
   const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage('');
+
     try {
       const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
@@ -31,12 +36,18 @@ const App = () => {
 
       const data = await response.json();
 
-      if(data.response == 'False') {
+      if(data.results == 'False') {
         setErrorMessage(data.Error || 'Failed to fetch movies');
+        setMovieList([]);
+        return;
       }
+
+      setMovieList(data.results || []);
     } catch (error) {
       console.log(`Error fetching movies: ${error}`);
       setErrorMessage('Error fetching movies. Please try again later.');
+    } finally {
+      setIsLoading(false);
     }
   }
 
@@ -59,11 +70,21 @@ const App = () => {
         <section className="all-movies">
           <h2>All Movies</h2>
 
-          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+          {isLoading ? (
+            <Spinner/>
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <p key={movie.id} className="text-white">{movie.title}</p>
+              ))}
+            </ul>
+          )}
         </section>
       </div>
     </main>
   )
 }
 
-export default App
+export default App;
